7248dcbe048ae206f7cac1985571f41c537839dd
thaihoc285 <thaihoc131005@gmail.com>
2025-11-14 23:16:30 +0700

    fix: fix ui ux fe

--- frontend/streamvod/src/components/Header/Header.jsx
+++ frontend/streamvod/src/components/Header/Header.jsx
@@ -1,24 +1,44 @@
 
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './Header.module.css';
 import { FiMenu, FiSearch, FiMic, FiUser } from 'react-icons/fi';
 import Logo from '../../assets/icons/logo.svg';
+import Sidebar from '../Sidebar/Sidebar';
 
 
 
 const Header = () => {
+  const navigate = useNavigate();
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const handleLogoClick = () => {
+    navigate('/');
+  };
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
   return (
-    <header className={styles.header}>
-      {/* left */}
-      <div className={styles.leftSection}>
-        <button className={`${styles.iconButton} ${styles.menuButton}`}>
-          <FiMenu size={24}/>     {/* */}
-        </button>
-        <div className={styles.logoContainer}>
-          { <img src={Logo} alt="StreamVN Logo" className={styles.logoIcon} />}
-          <span className={styles.logoText}>StreamVN</span>
+    <>
+      <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} />
+      
+      <header className={styles.header}>
+        {/* left */}
+        <div className={styles.leftSection}>
+          <button className={`${styles.iconButton} ${styles.menuButton}`} onClick={toggleSidebar}>
+            <FiMenu size={24}/>
+          </button>
+          <div className={styles.logoContainer} onClick={handleLogoClick}>
+            <img src={Logo} alt="StreamVN Logo" className={styles.logoIcon} />
+            <span className={styles.logoText}>StreamVN</span>
+          </div>
         </div>
-      </div>
 
       {/* giữa */}
       <div className={styles.centerSection}>
@@ -43,6 +63,7 @@ const Header = () => {
         </button>
       </div>
     </header>
+    </>
   );
 };
 
